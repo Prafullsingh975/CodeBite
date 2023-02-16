a6f4d467f3b95ab0ad2249e8eff80fc436fc5332
Prafull Singh <prafullsingh975@gmail.com>
2023-02-16 11:24:30 +0530

    Table booking

---
+++ script.js
@@ -0,0 +1,300 @@
+
+var rowData = [
+    {
+        cards:[
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            },
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            },
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            },
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            }
+        ]
+    },
+    {
+        cards:[
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            },
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            },
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            },{
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            }
+        ]
+    },
+    {
+        cards:[
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            },
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            },
+            {
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            },{
+                image: "https://images.pexels.com/photos/1743229/pexels-photo-1743229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                hotelName: "Radisson Hotel",
+                hoteladd: "Opposite Sivoy Complex, E8 Bawadiya Kalan,Gulmohar Bhopal",
+                wifi: "ri-wifi-line",
+                bed: "./icons/double-bed.png",
+                geyser: "./icons/geyser.png",
+                ac: "./icons/AC.png",
+                heater: "./icons/roomheater.png",
+                rating: "object",
+                price: "450INR/Night",
+                offer: "20%"
+            }
+        ]
+    }
+];
+var row = document.querySelector(".rowsHotel");
+var card = document.querySelector(".cards");
+var clutterRow = "";
+var count = 0;
+// rowData.forEach(function(val,index){
+//     console.log(val)
+//     let next =  val.cards
+//     console.log(next);
+// var clutter = ""
+//     next.forEach(function(val1,i){
+//     // console.log(val1.image);
+//     clutter += `
+//     <div class="rowHotel">
+//         <div class="cards">
+//         <img src="${val1.image}
+//             " alt="">
+//             <div class="cardcontent">
+//                 <h2>${val1.hotelName}</h2>
+//                 <h5>${val1.hoteladd}</h5>
+//                 <div class="cardicon">
+//                     <div class="icons">
+//                         <i class="${val1.wifi}"></i>
+//                         <h5>Wi-fi</h5>
+//                     </div>
+//                     <div class="icons">
+//                         <i><img src="${val1.bed}" alt=""></i>
+//                         <h5>Double Bed</h5>
+//                     </div>
+//                     <div class="icons">
+//                         <i><img src="${val1.geyser}" alt=""></i>
+//                         <h5>Geyser</h5>
+//                     </div>
+//                     <div class="icons">
+//                         <i><img id="AC" src="${val1.ac}" alt=""></i>
+//                         <h5>AC</h5>
+//                     </div>
+//                 </div>
+//                 <div class="cardicon">
+//                     <div class="icons">
+//                         <i><img src="${val1.heater}" alt=""></i>
+//                         <h5>Room heater</h5>
+//                     </div>
+//                 </div>
+//                 <div class="rating">
+//                     <h5>Rating:</h5>
+//                     <i class="ri-star-s-fill"></i>
+//                     <i class="ri-star-s-fill"></i>
+//                     <i class="ri-star-s-fill"></i>
+//                     <i class="ri-star-s-fill"></i>
+//                     <i class="ri-star-half-s-fill"></i>
+//                 </div>
+//                 <div class="price">
+//                     <h2>${val1.price}</h2>
+//                     <button>View Availability</button>
+//                 </div>
+//                 <div class="offer">
+//                     <h5>${val1.offer}OFF</h5>
+//                 </div>            
+//         </div>
+//     </div>
+//             `
+//             // count++;
+//             // console.log(count +" " + clutter)
+//             row.innerHTML = clutter;
+//     })
+//     // clutterRow = clutter;
+//     // console.log("outer" + count)
+//     row.innerHTML = clutterRow;
+
+// })
+
+rowData.forEach(function (val,index) {
+    console.log(val) // cards Container
+    let row = val.cards;
+    row.forEach(function (val1,i) {
+        console.log(val1); // Inner Html of row
+        // row.innerHTML = 
+    })
+})
+
+function ab(params) {
+    details.forEach(function(val1,index){
+        clutter += `
+            <img src="${val1.image}
+            " alt="">
+            <div class="cardcontent">
+                <h2>${val1.hotelName}</h2>
+                <h5>${val1.hoteladd}</h5>
+                <div class="cardicon">
+                    <div class="icons">
+                        <i class="${val1.wifi}"></i>
+                        <h5>Wi-fi</h5>
+                    </div>
+                    <div class="icons">
+                        <i><img src="${val1.bed}" alt=""></i>
+                        <h5>Double Bed</h5>
+                    </div>
+                    <div class="icons">
+                        <i><img src="${val1.geyser}" alt=""></i>
+                        <h5>Geyser</h5>
+                    </div>
+                    <div class="icons">
+                        <i><img id="AC" src="${val1.ac}" alt=""></i>
+                        <h5>AC</h5>
+                    </div>
+                </div>
+                <div class="cardicon">
+                    <div class="icons">
+                        <i><img src="${val1.heater}" alt=""></i>
+                        <h5>Room heater</h5>
+                    </div>
+                </div>
+                <div class="rating">
+                    <h5>Rating:</h5>
+                    <i class="ri-star-s-fill"></i>
+                    <i class="ri-star-s-fill"></i>
+                    <i class="ri-star-s-fill"></i>
+                    <i class="ri-star-s-fill"></i>
+                    <i class="ri-star-half-s-fill"></i>
+                </div>
+                <div class="price">
+                    <h2>${val1.price}</h2>
+                    <button>View Availability</button>
+                </div>
+                <div class="offer">
+                    <h5>${val1.offer}OFF</h5>
+                </div>
+            </div>`
+            card.innerHTML = clutter;
+    })
+}
